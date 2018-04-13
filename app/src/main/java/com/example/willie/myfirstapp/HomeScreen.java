4ff9add413d005da3dd1d311a730a2e437447bf6
Sandaimei <33132460+Sandaimei@users.noreply.github.com>
2018-04-12 20:17:51 -0500

    Retrieve User Data
    Added ability to retrieve user data from the database. I know how to do it now so we can retireve what we need whenever

--- app/src/main/java/com/example/willie/myfirstapp/HomeScreen.java
+++ app/src/main/java/com/example/willie/myfirstapp/HomeScreen.java
@@ -1,23 +1,50 @@
 package com.example.willie.myfirstapp;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class HomeScreen extends AppCompatActivity {
 
     private FirebaseAuth firebaseAuth;
+    private FirebaseDatabase firebaseDatabase;
+    private TextView profileName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homescreen);
 
+        profileName = findViewById(R.id.tvProfileName);
+
         firebaseAuth = FirebaseAuth.getInstance();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+
+        @SuppressLint("RestrictedApi")DatabaseReference databaseReference = firebaseDatabase.getReference(firebaseAuth.getUid());
+        databaseReference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                UserProfile userProfile = dataSnapshot.getValue(UserProfile.class);
+                profileName.setText("HI " + userProfile.getUserFirst_Name() + "!");
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Toast.makeText(HomeScreen.this, databaseError.getCode(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
--- app/src/main/java/com/example/willie/myfirstapp/UserProfile.java
+++ app/src/main/java/com/example/willie/myfirstapp/UserProfile.java
@@ -7,6 +7,8 @@ public class UserProfile {
     public String Email;
     public String Age;
 
+    public UserProfile() {};
+
     public UserProfile(String First_Name, String Last_Name, String Email, String Age) {
         this.First_Name = First_Name;
         this.Last_Name = Last_Name;
@@ -14,5 +16,35 @@ public class UserProfile {
         this.Age = Age;
     }
 
-    
+    public String getUserFirst_Name() {
+        return First_Name;
+    }
+
+    public void setUserFirst_Name(String first_Name) {
+        First_Name = first_Name;
+    }
+
+    public String getUserLast_Name() {
+        return Last_Name;
+    }
+
+    public void setUserLast_Name(String last_Name) {
+        Last_Name = last_Name;
+    }
+
+    public String getUserEmail() {
+        return Email;
+    }
+
+    public void setUserEmail(String email) {
+        Email = email;
+    }
+
+    public String getUserAge() {
+        return Age;
+    }
+
+    public void setUserAge(String age) {
+        Age = age;
+    }
 }
